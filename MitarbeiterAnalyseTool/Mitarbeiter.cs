3456a75cdcf70eee4e3636e80e3af5a8746a7bc3
Azlan Ainto <azlanainto97@gmail.com>
2026-08-29 22:15:28 +0200

    Projektdateien hinzufügen.

---
+++ MitarbeiterAnalyseTool/Mitarbeiter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MitarbeiterAnalyseTool
+{
+    public class Mitarbeiter
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+        public decimal Gehalt {  get; set; }
+
+        public Mitarbeiter(string vorname, 
+                           string nachname,
+                           DateTime geburtsdatum,
+                           decimal gehalt)
+        {
+            this.Vorname = vorname;
+            this.Nachname = nachname;
+            this.Geburtsdatum = geburtsdatum;
+            this.Gehalt = gehalt;
+
+
+        }
+
+
+        public void DatenAusgeben()
+        {
+            Console.WriteLine("--- Mitarbeiter Details ---\n");
+            Console.WriteLine($"Name:\t\t{Vorname} {Nachname}");
+            Console.WriteLine($"Geburtsdatum:\t{Geburtsdatum:d}");      // d == .ToShortDateString()
+
+            Console.WriteLine($"Gehalt:\t\t{Gehalt} Euro");
+        }
+
+    }
+}
---
+++ MitarbeiterAnalyseTool/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MitarbeiterAnalyseTool
+{
+    public class MitarbeiterAnalyseTool
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Willkommen beim Mitarbeiter-Analyse-Tool!");
+            Console.WriteLine();
+
+            DateTime geburtsdatum = new DateTime(1986, 12, 2);
+            Mitarbeiter m1 = new Mitarbeiter("Max",
+                                            "Mustermann",
+                                             geburtsdatum,
+                                             3500.50m);
+            m1.DatenAusgeben();
+
+            Console.WriteLine();
+            Console.WriteLine("Drücke eine beliebige Taste zum Beenden...");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
