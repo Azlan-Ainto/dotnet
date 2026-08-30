24bdfd997faa36f981d8ad74d6ce0e1b69316a63
Azlan Ainto <azlanainto97@gmail.com>
2026-08-30 15:38:54 +0200

    Klasse MitarbeiterVerwaltung erstellt. Anpassungen der Klasse Programm.

---
+++ MitarbeiterAnalyseTool/MitarbeiterVerwaltung.cs
@@ -0,0 +1,58 @@
+﻿using static System.Console;
+
+namespace MitarbeiterAnalyseTool
+{
+    public class MitarbeiterVerwaltung
+    {
+        private readonly List<Mitarbeiter> mitarbeiterListe;
+
+        public MitarbeiterVerwaltung()
+        {
+            mitarbeiterListe = new List<Mitarbeiter>();
+        }
+
+        public void MitarbeiterHinzufuegen(Mitarbeiter neuerMitarbeiter)
+        {
+            mitarbeiterListe.Add(neuerMitarbeiter);
+            WriteLine($"Mitarbeiter {neuerMitarbeiter.Vorname} " +
+                              $"{neuerMitarbeiter.Nachname} " +
+                              $"wurde erfolgreich hinzugefügt.");   
+        }
+
+
+
+        public void AlleMitarbeiterAusgeben()
+        {
+            WriteLine("\n--- Komplette Mitarbeiterliste ---");
+            if (mitarbeiterListe.Count == 0)
+            {
+                WriteLine("Kein Mitarbeiter in der Datenbank vorhanden.");
+                return;
+            }
+
+            foreach (Mitarbeiter aktuellerMitarbeiter in mitarbeiterListe)
+            {
+                aktuellerMitarbeiter.DatenAusgeben();
+            }
+        }
+
+        public decimal DurchschnittsgehaltsBerechnen()
+        {
+            if (mitarbeiterListe.Count == 0)
+            {
+                return 0m;
+            }
+
+            decimal gesamtGehalt = 0m;
+
+            foreach(Mitarbeiter aktuellerMitarbeier in mitarbeiterListe)
+            {
+                gesamtGehalt += aktuellerMitarbeier.Gehalt;
+            }
+
+            return gesamtGehalt / mitarbeiterListe.Count;
+        }
+
+
+    }
+}
--- MitarbeiterAnalyseTool/Program.cs
+++ MitarbeiterAnalyseTool/Program.cs
@@ -1,23 +1,61 @@
-﻿namespace MitarbeiterAnalyseTool
+﻿using System.Threading.Channels;
+
+namespace MitarbeiterAnalyseTool
 {
     public class MitarbeiterAnalyseTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Willkommen beim Mitarbeiter-Analyse-Tool!");
+            
+            MitarbeiterVerwaltung verwaltung = new();
+
+            Mitarbeiter m1 = new("Max", "Mustermann", new DateTime(1986, 12, 02), 5000.00m);
+            Mitarbeiter m2 = new("Anna", "Schmidt", new DateTime(1990, 12, 2), 4000.90m);
+            Mitarbeiter m3 = new("Thomas", "Mann", new DateTime(1950,12,2), 4500.50m);
+
+            verwaltung.MitarbeiterHinzufuegen(m1);
+            verwaltung.MitarbeiterHinzufuegen(m2);
+            verwaltung.MitarbeiterHinzufuegen(m3);
+            verwaltung.AlleMitarbeiterAusgeben();
+
+            decimal durchschnittsgehalt = verwaltung.DurchschnittsgehaltsBerechnen();
+            Console.WriteLine($"\nDurchschnittsgehalt beträgt: {durchschnittsgehalt:N2} EUR ");
+            Console.WriteLine();
             Console.WriteLine();
 
-            DateTime geburtsdatum = new DateTime(1986, 12, 2);
-            Mitarbeiter m1 = new Mitarbeiter("Max",
-                                            "Mustermann",
-                                             geburtsdatum,
-                                             3500.50m);
-            m1.DatenAusgeben();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"=======================================================\n");
+            Console.ResetColor();
 
-            Console.WriteLine();
-            Console.WriteLine("Drücke eine beliebige Taste zum Beenden...");
-            Console.WriteLine();
+            // List<Mitarbeiter> mitarbeiterListe = new List<Mitarbeiter>();
+            List<Mitarbeiter> mitarbeiterListe =
+            [
+                  new("Max", "Meier", new DateTime(1986, 12, 02), 5000.00m),
+                  new("Anna", "Anton", new DateTime(1990, 12, 2), 4000.90m),
+                  new("Tim", "Tuchel", new DateTime(1950,12,2), 4500.50m),
+                  new("Bart", "Baum", new DateTime(1986,12,2), 3500.10m),
+                  new("Claudia", "Clark", new DateTime(1986,12, 2), 4800.20m),
+                  new("Claus","Clown", new DateTime(1986,12,3),4900.30m)
+            ];
+            foreach(var  m in mitarbeiterListe)
+            {
+                verwaltung.MitarbeiterHinzufuegen(m);
+            }
+            verwaltung.AlleMitarbeiterAusgeben();
+            decimal gehaltmittelwert = verwaltung.DurchschnittsgehaltsBerechnen();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write($"\n\nDer Gehaltsdurchschnitt beträgt:");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($" {gehaltmittelwert:N2} EUR\n");
+            Console.ResetColor();
+
+            Console.WriteLine("Drücke eine beliebige Taste zum Beenden.");
             Console.ReadKey();
+            
+
+       
         }
     }
 }
